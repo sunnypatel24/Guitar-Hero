b81a04a2d86d7bb5bcc14ba5cc48c1c43f9b684e
Sunny <sunnypatel2000@yahoo.com>
2020-07-25 21:19:00 -0500

    Add files via upload

---
+++ GuitarString.java
@@ -0,0 +1,87 @@
+/**
+ * CS312 Assignment 12.
+ *
+ * On MY honor, Sunny Patel, this programming assignment is MY own work and I
+ * have not provided this code to any other student.
+ *
+ * Student name: Sunny Patel
+ *  UTEID: sgp736 
+ * email address: sunnypatel2000@utexas.edu
+ *  Grader name: Shreyas
+ *  Number of slip days used on this assignment: 0
+ *
+ */
+
+// A class to create vibrating guitar strings in order to simulate a guitar.
+public class GuitarString {
+    private static final int samplingRate = 44100;
+    private static final double randomValueScale = 0.5;
+    private static final double decayFactor = 0.994;
+    private static final double average = 0.5;
+
+    private double frequency;
+    private int numTics;
+    private int desiredCapacity;
+    private RingBuffer ringBuffer;
+
+    /* Constructor that creates a guitar string of the given frequency, using a sampling rate of 
+     * 44,100.
+     * @param frequency Sound frequency used to get the desired capacity for the ringbuffer.
+    */
+    public GuitarString(double frequency) {
+        this.frequency = frequency;
+        this.numTics = 0;
+        this.desiredCapacity = (int) Math.round((samplingRate / frequency));
+        this.ringBuffer = new RingBuffer(desiredCapacity);
+        for (int bufferIndex = 0; bufferIndex < desiredCapacity; bufferIndex++) {
+            ringBuffer.enqueue(0);
+        }
+    }
+
+
+    /* Creates a guitar string whose size and initial values are given by the array.
+     * @param init Array used to initialize the size of the ringbuffer and the values within it.
+    */
+    public GuitarString(double[] init) {
+        ringBuffer = new RingBuffer(init.length);
+        for (int index = 0; index < init.length; index++) {
+            ringBuffer.enqueue(init[index]);
+        }
+    }
+
+
+    // Sets the buffer to white noise.
+    public void pluck() {
+        for (int bufferIndex = 0; bufferIndex < desiredCapacity; bufferIndex++) {
+            double replaceNum = Math.random() - randomValueScale;
+            ringBuffer.dequeue();
+            ringBuffer.enqueue(replaceNum);
+        }
+    }
+
+
+    // Advances the simulation one time step.
+    public void tic() {
+        numTics++;
+        double frontSample = ringBuffer.dequeue();
+        double newFrontValue = ringBuffer.peek();
+        double valueAtEnd = decayFactor * average  * (frontSample + newFrontValue);
+        ringBuffer.enqueue(valueAtEnd);
+    }
+
+    /* Returns the current sample.
+     * @return Returns the value at the front of the buffer.
+    */
+    public double sample() {
+        return ringBuffer.peek();
+    }
+
+
+    /* Returns the total number of times tic() was called.
+     * @return numTics Holds the number of times the tic() method was called.
+    */
+    public int time() {
+        return numTics;
+    }
+
+}
---
+++ RingBuffer.java
@@ -0,0 +1,139 @@
+import java.util.NoSuchElementException;
+
+/**
+* CS312 Assignment 12.
+*
+* On MY honor, Sunny Patel, this programming assignment is MY own work
+* and I have not provided this code to any other student.
+*
+* Student name: Sunny Patel
+* UTEID: sgp736
+* email address: sunnypatel2000@utexas.edu
+* Grader name: Shreyas
+* Number of slip days used on this assignment: 0
+*
+*/
+
+// A class to represent a ring buffer data structure.
+public class RingBuffer {
+    private static final String EMPTY_BUFFER = "[]";
+
+    private double bufferQueue[];
+    private int capacity;
+    private int first;
+    private int last;
+    private int currentSize;
+
+    /* Constructor that initializes the double array to represent the ring buffer, along with first
+     * and last indices and currentSize holder.
+     * @param capacity The maximum size that the ring buffer is initialized to.
+    */
+    public RingBuffer(int capacity) {
+        this.capacity = capacity;
+        this.bufferQueue = new double[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.currentSize = 0;
+    }
+
+
+    /* return number of items currently in the buffer
+     * @return currentSize Holds the amount of values that are currently in the buffer.
+    */
+    public int size() {
+        return currentSize;
+    }
+
+
+    /* Checks if the buffer is empty (size equals zero).
+     * @return Returns true if currentSize is 0, and false if greater than 0.
+    */
+    public boolean isEmpty() {
+        return (currentSize == 0);
+    }
+
+
+    /* Checks if the buffer is full (size equals capacity).
+     * @return Returns true if the currentSize is equal to the maximum capacity, and false if it is
+     * less.
+    */
+    public boolean isFull() {
+        return (currentSize == capacity);
+    }
+
+
+    /* Adds the value x to the end of the buffer (as long as the buffer is not full).
+     * @param x Double value that is to be put into the queue of the buffer (at index last).
+    */
+    public void enqueue(double x) throws IllegalStateException {
+        boolean currentlyFull = isFull();
+        if (!currentlyFull) {
+            bufferQueue[last] = x;
+            currentSize++;
+            last++;
+            if (last == capacity) {
+                last = 0;
+            }
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+
+    /* Deletes and returns the value from the front of the buffer (as long as the buffer is not
+     * empty).
+     * @return firstNum The value that was removed from the queue.
+    */
+    public double dequeue() throws NoSuchElementException {
+        boolean currentlyEmpty = isEmpty();
+        double firstNum = bufferQueue[first];
+        if (!currentlyEmpty) {
+            first++;
+            currentSize--;
+            if (first == capacity) {
+                first = 0;
+            }
+            return firstNum;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+
+    /* Returns (but does not delete) the value from the front of the buffer.
+     * @return Returns the value that is at index first in the buffer.
+    */
+    public double peek() throws NoSuchElementException {
+        boolean currentlyEmpty = isEmpty();
+        if (!currentlyEmpty) {
+            return bufferQueue[first];
+        } else {
+            throw new NoSuchElementException();
+        }
+        
+    }
+
+
+    /* Returns a String of the form [front, next, next, last] (all of the values from first
+     * (inclusive) to last (exclusive)).
+     * @return queueOrder A string containing the order of the values currently in queue in the
+     *  buffer.
+     * @return EMPTY_BUFFER A string of empty brackets to indicate that there is no value currently
+     *  in queue.
+    */
+    public String toString() {
+        if (currentSize == 0) {
+            return EMPTY_BUFFER;
+        } else {
+            String queueOrder = "";
+            queueOrder += "[" + bufferQueue[first];
+            for (int bufferIndex = first + 1; bufferIndex < first + currentSize; bufferIndex++) {
+                queueOrder += ", ";
+                queueOrder += bufferQueue[bufferIndex % capacity];
+            }
+            queueOrder += "]";
+            return queueOrder;
+        }
+    }
+
+}
